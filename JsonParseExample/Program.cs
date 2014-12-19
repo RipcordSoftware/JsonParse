69d2bafe8319a8cb26e5b9826d6520d235c94fd4
RipcordSoftware <craig@ripcordsoftware.com>
2014-12-19 15:12:00 +0000

    Corrected example project commit

--- JsonParseExample/Program.cs
+++ JsonParseExample/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+using RipcordSoftware.JsonParse;
+
 namespace JsonParseExample
 {
     class MainClass
     {
+        private static string json = 
+            @"{""menu"": { ""id"": ""file"", ""value"": ""File"", ""popup"": { ""menuitem"": [
+                {""value"": ""New"", ""onclick"": ""CreateNewDoc()""},
+                {""value"": ""Open"", ""onclick"": ""OpenDoc()""},
+                {""value"": ""Close"", ""onclick"": ""CloseDoc()""}
+            ]}}}";
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser = new JsonParse("/menu/popup/menuitem/*", "value");
+            parser.Parse(json);
+
+            foreach (JsonIdentityExtent extent in parser.MatchedExtents)
+            {
+                Console.WriteLine("Extent: {0}, Identity: {1}", extent, extent.IdentityExent);
+            }
         }
     }
 }
