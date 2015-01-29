e063c6292c247709f8f7a86da6dad57f0004b273
RipcordSoftware <craig@ripcordsoftware.com>
2015-01-29 23:09:28 +0000

    Moved example JSON tests into seperate file

--- JsonParseTest/Test.cs
+++ JsonParseTest/Test.cs
@@ -76,36 +76,6 @@ namespace JsonParseTest
         {
             var json = "{\"x\":10, \"y\":\"abc\", \"z\":[}";
             JsonParse.Parse(json, string.Empty);
-        }
-
-        [Test()]
-        public void TestExample1()
-        {
-            JsonParse.Parse(Examples.exampleJson1, string.Empty);
-        }
-
-        [Test()]
-        public void TestExample2()
-        {
-            JsonParse.Parse(Examples.exampleJson2, string.Empty);
-        }
-
-        [Test()]
-        public void TestExample3()
-        {
-            JsonParse.Parse(Examples.exampleJson3, string.Empty);
-        }
-
-        [Test()]
-        public void TestExample4()
-        {
-            JsonParse.Parse(Examples.exampleJson4, string.Empty);
-        }
-
-        [Test()]
-        public void TestExample5()
-        {
-            JsonParse.Parse(Examples.exampleJson5, string.Empty);
-        }
+        }            
     }
 }
---
+++ JsonParseTest/TestExamples.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+using RipcordSoftware.JsonParse;
+
+namespace JsonParseTest
+{
+    [TestFixture()]
+    public class TestExamples
+    {
+        [Test()]
+        public void TestExample1()
+        {
+            JsonParse.Parse(Examples.exampleJson1, string.Empty);
+        }
+
+        [Test()]
+        public void TestExample2()
+        {
+            JsonParse.Parse(Examples.exampleJson2, string.Empty);
+        }
+
+        [Test()]
+        public void TestExample3()
+        {
+            JsonParse.Parse(Examples.exampleJson3, string.Empty);
+        }
+
+        [Test()]
+        public void TestExample4()
+        {
+            JsonParse.Parse(Examples.exampleJson4, string.Empty);
+        }
+
+        [Test()]
+        public void TestExample5()
+        {
+            JsonParse.Parse(Examples.exampleJson5, string.Empty);
+        }
+    }
+}
+
