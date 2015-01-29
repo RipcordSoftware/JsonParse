f8d88d3e60f6b167ad0e247469fd03c20f7a03f8
RipcordSoftware <craig@ripcordsoftware.com>
2015-01-29 23:15:30 +0000

    Moved malformed test cases into their own class

---
+++ JsonParseTest/TestMalformed.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+
+using RipcordSoftware.JsonParse;
+
+namespace JsonParseTest
+{
+    [TestFixture()]
+    public class TestMalformed
+    {
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON1()
+        {
+            var json = "{x:10}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON2()
+        {
+            var json = "{\"x\":abc}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON3()
+        {
+            var json = "{\"x\":10 \"y\":20}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON4()
+        {
+            var json = "{\"x:10}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON5()
+        {
+            var json = "{\"x\":10, \"y\":\"abc}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON6()
+        {
+            var json = "{\"x\":10, \"y\" \"abc\"}";
+            JsonParse.Parse(json, string.Empty);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(JsonParseException))]
+        public void TestMalformedJSON7()
+        {
+            var json = "{\"x\":10, \"y\":\"abc\", \"z\":[}";
+            JsonParse.Parse(json, string.Empty);
+        }   
+    }
+}
+
